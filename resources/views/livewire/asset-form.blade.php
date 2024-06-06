f07b2e104b6630090eeaf551cb1cfb09b5329cea
LucianoBeltrao <lucianoalvesjf@gmail.com>
2024-06-06 01:54:13 +0000

    adicionando cabeçalho nos formulários

--- resources/views/livewire/asset-form.blade.php
+++ resources/views/livewire/asset-form.blade.php
@@ -2,6 +2,13 @@
     <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
         <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
+            <p
+                class="text-3xl font-medium text-orange-500  bg-gradient-to-r from-secondary-300 via-secondary-400 to-secondary-400 dark:text-white container mx-auto px-4 py-5">
+                Novo Ativo
+
+                
+            </p>
+
             <div class="flex items-right justify-end py-2">
 
                 <a href="{{ route('assets') }}"
